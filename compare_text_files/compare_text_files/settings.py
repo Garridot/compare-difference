28867416e0538394ed4275628577934157ab6ea4
Garridot <garridot210@gmail.com>
2022-02-14 17:45:49 -0300

    requirements.txt generate

--- compare_text_files/compare_text_files/settings.py
+++ compare_text_files/compare_text_files/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-i30w9mgyjg%1kx+vx^&ku#zrdxxxr+=vdswfa#p*h)g9a@+ia7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
